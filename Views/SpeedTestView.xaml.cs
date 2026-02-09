7f47b620c687878c2e63e43b7c1c1282e9c85b63
Jackson <jjfbno@protonmail.com>
2026-02-08 21:01:44 -0700

    Add files via upload

---
+++ Views/SpeedTestView.xaml.cs
@@ -0,0 +1,22 @@
+using System.Windows;
+using System.Windows.Controls;
+using JFStorageTester.ViewModels;
+
+namespace JFStorageTester.Views;
+
+public partial class SpeedTestView : UserControl
+{
+    public SpeedTestView()
+    {
+        InitializeComponent();
+        Loaded += OnLoaded;
+    }
+    
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        if (Application.Current?.MainWindow?.DataContext is MainViewModel mainVm)
+        {
+            DataContext = mainVm.SpeedTestViewModel;
+        }
+    }
+}
